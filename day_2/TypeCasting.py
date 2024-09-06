72379e1ef85d17e0e9adbe5d01a2894e199e505b
HearSight Unit2 <160222903+NisathAndroid@users.noreply.github.com>
2024-09-06 21:38:14 +0530

    day-3: string quotes

--- day_2/TypeCasting.py
+++ day_2/TypeCasting.py
@@ -9,8 +9,15 @@ print("convert int value=",convert_int,"type =",type(convert_int))
 
 salary = '18000.0' #str
 print(2,type(salary)) #string
-convert_int = int(salary) #ValueError: invalid literal for int() with base 10: '18000.0'
-print(convert_int ,type(convert_int))
+# convert_int = int(salary) #ValueError: invalid literal for int() with base 10: '18000.0'
+# print(convert_int ,type(convert_int))
 
 salary = 0.18000j #complex
 print(3,type(salary)) #
+
+complex = 12+3j
+type_of =str(complex)
+print(1,"->",type(type_of),type_of)
+type_of = float(complex.real)
+
+print(2,'->',type(type_of),type_of)
---
+++ day_3/AboutStrings.py
@@ -0,0 +1,29 @@
+# single quotes -> 'your content'
+# double quotes -> " your content "
+# Multiline quotes -> """ your content """ or ''' your content '''
+
+
+your_content = 'my name is khan'
+
+print(1,"->",your_content)
+
+your_content = "my name is khan"
+
+print(2,"->",your_content)
+
+#Quotes Inside Quotes
+
+your_content = ('my name is '
+                'h'
+                'h'
+                'f'
+                '"khan" ')
+
+print(3,"->",your_content)
+
+your_content = '''my
+ name 
+is "khan" 
+'''
+
+print(4,"->",your_content)
