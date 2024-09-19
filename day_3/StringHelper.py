c713b19b14a518d7122a5c070293f76f87d7e76d
HearSight Unit2 <160222903+NisathAndroid@users.noreply.github.com>
2024-09-19 21:16:58 +0530

    f-string , slice , concadinate

--- day_3/StringHelper.py
+++ day_3/StringHelper.py
@@ -15,9 +15,14 @@ print("not in =",checkName not in story)
 print("in =",checkName  in story)
 
 #slicing
+name = "floor"
 print("slicing =",story[8:15]) #in between
 print("slice start =",story[12:]) #from this index
 print("slice end =",story[:15]) #upto this index-1
+#Negative Indexing
+print("index move on Right to Left slice =",name[-3:-1]) # f-5 l-4 o-3 o-2 r-1
+#Positive Indexing
+print("index move Left to Right slice =",name[2:4]) # f=0 l=1 o=2 o=3 r=4
 
 # upper(),lower(),strip(),replace(),split()
 print("upper =",story.upper())
@@ -28,4 +33,28 @@ phone =""" +918978778723,+918922234343,+919845755855,+9174455454154,+91656598989
 
 print("split =",phone.split(","))
 
+#String Concatenation
+firstName ="nandhini"
+lastName = "rajendhiran"
+fullName = firstName + " " + lastName
+print("full name = ",fullName)
+
+std = 11
+sec ="A"
+#c =std + sec =======>>>>> #TypeError: unsupported operand type(s) for +: 'int' and 'str'
+c = str(std) + sec
+print(c)
+
+#F-Strings
+c= f"my class name :{sec}{std}"
+print(c)
+
+#f-string with decimal
+
+price = 67.8
+text = f"apple rate is {price:.5f}"
+print(text)
+
+txt = f"The \"price\" is {10 * 5} dollars"
+print(txt)
 
